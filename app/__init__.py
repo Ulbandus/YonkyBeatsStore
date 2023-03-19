9c6ae91f142f348c5a1cb30a3a945fdc294151bb
Ulbandus <tovarishbozhanov@gmail.com>
2023-03-19 20:36:16 +0300

    added login page and other minor stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
+
 app = Flask(__name__, template_folder='./templates/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+
 
 from app import routes
+
--- run.py
+++ run.py
@@ -2,4 +2,8 @@
 from app import app
 from os import environ
 
+
 app.run(port=environ.get("PORT", 5000), host='0.0.0.0', debug=True)
+
+
+
